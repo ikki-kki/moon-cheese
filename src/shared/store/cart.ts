4ff2fa0e269dbc738e14b1b49095bceb410c1e28
성정민 <hellojeongmin@gmail.com>
2026-01-31 21:39:11 +0900

    fix: cart 스토어 로직 세부화

--- src/shared/store/cart.ts
+++ src/shared/store/cart.ts
@@ -2,18 +2,53 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 import type { Product } from '../api/schema';
 
+export type CartItem = Product & {
+  quantity: number;
+};
+
 interface CartStore {
-  products: Product[];
-  addToCart: (product: Product) => void;
+  cartItems: CartItem[];
+  addToCart: (product: Product, quantity: number) => void;
   removeFromCart: (productId: number) => void;
+  increaseQuantity: (productId: number) => void;
+  decreaseQuantity: (productId: number) => void;
 }
 
-export const useCartStore = create(
-  persist<CartStore>(
+export const useCartStore = create<CartStore>()(
+  persist(
     set => ({
-      products: [],
-      addToCart: product => set(state => ({ products: [...state.products, product] })),
-      removeFromCart: id => set(state => ({ products: state.products.filter(p => p.id !== id) })),
+      cartItems: [],
+
+      addToCart: (product, quantity) =>
+        set(state => {
+          const isExist = state.cartItems.some(item => item.id === product.id);
+
+          if (isExist) {
+            return {
+              cartItems: state.cartItems.map(item =>
+                item.id === product.id ? { ...item, quantity: item.quantity + quantity } : item
+              ),
+            };
+          }
+          return { cartItems: [...state.cartItems, { ...product, quantity }] };
+        }),
+
+      removeFromCart: id =>
+        set(state => ({
+          cartItems: state.cartItems.filter(item => item.id !== id),
+        })),
+
+      increaseQuantity: id =>
+        set(state => ({
+          cartItems: state.cartItems.map(item => (item.id === id ? { ...item, quantity: item.quantity + 1 } : item)),
+        })),
+
+      decreaseQuantity: id =>
+        set(state => ({
+          cartItems: state.cartItems
+            .map(item => (item.id === id ? { ...item, quantity: item.quantity - 1 } : item))
+            .filter(item => item.quantity > 0),
+        })),
     }),
     {
       name: 'cart-store',
